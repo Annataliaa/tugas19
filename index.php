f48c64b038224cbb5be02ca0f061618ff1efdf24
Annataliaa <annatalia66842@gmail.com>
2023-05-11 19:29:37 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 19</title>
+</head>
+<body>
+    <center>
+    <h1>Tugas Pertemuan 19</h1>
+    <br>
+    <br>
+    <br>
+    <br>
+    <h2>Menu Utama</h2>
+    <h3>Annatalia</h3>
+    <h3><p>Full Stack Web Development 2</p></h3>
+    <h3><p>4561436</p></h3>
+        <li><a href="listUser.php">List User</a></li>
+        <li><a href="createUser.php">Create User</a></li>
+    <center>
+</body>
+</html>
