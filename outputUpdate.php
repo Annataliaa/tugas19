55409b4c476c2628ba3abfd0d5a6700016abcd3f
Annataliaa <annatalia66842@gmail.com>
2023-05-14 19:33:21 +0700

    FinishP1920

--- outputUpdate.php
+++ outputUpdate.php
@@ -4,43 +4,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data User Baru</title>
+    <title>Update User</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Halaman Hasil Penambahan User</h1>
-<?php
-if(isset($_POST['submit']) && isset($_FILES['avatar'])){
-    include "connectDB.php";
+    <h1>Hasil Edit User</h1>
+    <?php
+    if(isset($_POST['submit']) && isset($_FILES['avatar'])){
+        include "connectDB.php";
 
-    $id = $_POST['id'];
-    $name = $_POST['name'];
-    $role = $_POST['role'];
-    $password = $_POST['password'];
-    $email = $_POST['email']; 
-    $phone = $_POST['phone'];
-    $address = $_POST['address'];
-    $avatar = $_FILES['avatar']['name'];
-    $avatar_tmp = $_FILES['avatar']['tmp_name'];
+        $id = $_GET['id'];
+        $name = $_POST['name'];
+        $role = $_POST['role'];
+        $password = $_POST['password'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $address = $_POST['address'];
+        $avatar = $_FILES['avatar']['name'];
+        $avatar_tmp = $_FILES['avatar']['tmp_name'];
 
-    $img_ex = pathinfo($avatar, PATHINFO_EXTENSION);
-    $img_ex_lc = strtolower($img_ex);
-    $avatarName = uniqid("IMG-", true).'.'.$img_ex_lc;
+        $img_ex = pathinfo($avatar, PATHINFO_EXTENSION);
+        $img_ex_lc = strtolower($img_ex);
+        $avatarName = uniqid("IMG-", true).'.'.$img_ex_lc;
+        
+        $sql_update = "UPDATE user SET email = '$email', name = '$name', `role` = '$role', avatar = '$avatarName', phone = '$phone', address = '$address', password = '$password', updated_at = DEFAULT WHERE id = $id;";
 
-    $sql_post = "INSERT INTO user (id, email, name, `role` , avatar, phone, address, password , created_at, updated_at) VALUES ('$id', '$email', '$name', '$role', '$avatarName', '$phone', '$address', '$password', DEFAULT, DEFAULT);";
+        if ($conn->query($sql_update) === TRUE) {
+            echo "<h2>Edit user Successfully </h2> <br>";
+            $img_upload_path = 'image/'. $avatarName;
+            move_uploaded_file($avatar_tmp, $img_upload_path);
+        } else {
+            echo "Error: " . $sql_update . "<br>" . $conn->error;
+        }
 
-    if ($conn->query($sql_post) === TRUE) {
-        echo "<h2>Data User Baru Berhasil Ditambahkan</h2><br>";
-        $img_upload_path = 'image/'. $avatarName;
-        move_uploaded_file($avatar_tmp, $img_upload_path);
-    } else {
-        echo "Error: " . $sql_post . "<br>" . $conn->error;
-    }
+        $sql_read = "select * from user where id = $id";
+        $result = $conn->query($sql_read);
 
-    $sql_read = "select * from user where id = $id";
-    $result = $conn->query($sql_read);
-    $row = mysqli_fetch_array($result);
-    echo "<table class='table'>
+        $row = mysqli_fetch_array($result);
+        echo "<table class='table'>
         <thead>
             <tr class='table-primary'>
                 <th scope='col'>ID</th>
@@ -74,11 +75,11 @@ if(isset($_POST['submit']) && isset($_FILES['avatar'])){
                 <td>" . $row['created_at'] . "</td>
                 <td>" . $row['updated_at'] . "</td>
             </tr>";
-}
-else {
-    echo "error";
-}
-?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    }
+    else {
+        echo "error";
+    }
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
